7035da2bd6e08a51879bfc8bf1ef29de6900ac91
cherepanko <109464961+cherepanko@users.noreply.github.com>
2022-08-28 13:05:15 +0300

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,14 +1,14 @@
 /* eslint-disable no-unused-vars */
 
-export const GROUP_TOKEN = 'vk1.a.4WaprSkLWR1_2Tx9BEOC5JRMnBxia5oT1iy37KF23iLuH7JB7KsBqbHOQlb_5viuA7CyGi1nkX_tJK_T4-R3vz7KTyTjPkrQQrGEIyRUW80cvuyabWnxT3rmogEKkyovvPORaJ5GLmeFSbVIBCnCLu0zKAAOtIIYnG3qhfzKE22mpRpLpwX8_wkoFWe-3QkS';
-export const USER_TOKEN = 'vk1.a.7BuvhCe9VbBwAVHU6AyLR14ilZXxQoen13Iz-fKOE5NtsY1GrVcx6yTCGDpvyYRskh0jfasfHPlYy-WJxJ_FKFZ5DLDGhXPkyqONmmhTDHbk3bmL4KJHhQkMQ3aSEtm6ma_kDg9oBFPlnwxujX1of4iNcBkag7gZCOdMGfhrIotTB4OOLB9aNeGew2S1M_Zg';
-export const GROUP_ID = 213090791;
-export const ADMINS = [703295904];
+export const GROUP_TOKEN = '4f30550665c37057eb00f0496e56c0a2079a5fb23dba5d97c1ba5746c52db986793bc96f11a16b3d8fe8f';
+export const USER_TOKEN = 'vk1.a.yXyNRqHZcQvy5PspMtbtL30T2PkU93LR_bKIBvKlCutpj47Wp_7ivqJojDMH3y82J-AcUekGhO2dzBpO9-fbB4_mWEsqiAB6GcmO5hyf__Asjn_JO7Z1lYX73eaOe6KPndVwwJKy0EcVk57orcnRiUuKxjzWP_Cqd0_LU3qNHruJcA19i2iNHBz-ucH8kXNn';
+export const GROUP_ID = 214759030;
+export const ADMINS = [ 703295904 ];
 export const SERVICE_KEY = 'c31d8c8ec31d8c8ec31d8c8e71c36001bccc31dc31d8c8ea1c24b8cae02f5d32aa17556';
 
 export const DB_USER = 'admin';
 export const DB_PASSWORD = 'maksimka';
-export const DB_NAME = 'loverysing';
+export const DB_NAME = 'loverysing34';
 
 export const SECRET_KEY = '0Zw7kvmgVhyv7yzAxVhs';
 
